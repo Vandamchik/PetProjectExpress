3616be77d945f55d51e90ea616a697946f9756c7
Vandamchik <cherepcock@gmail.com>
2023-10-19 14:49:18 +0300

    maked regestration logic, connect to mongo,maked decomposition, set refreshToken in cookie

--- service/users-service.js
+++ service/users-service.js
@@ -1,20 +1,28 @@
-const UserModel = require('../models/users-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
+
+const UserModel = require('../models/users-model');
 const mailService = require('./mail-service');
+const tokenService = require('./token-service');
 const HttpError = require('../models/http-error');
+const UserDto = require('../dtos/user-dto');
 
 
 class UsersService {
     async registration(email, password) {
         const singleUser = await UserModel.findOne({ email });
         if (singleUser) {
-            throw new HttpError(`User with ${ email } is already exist`, 409);
+            return new HttpError(`User with ${ email } is already exist`, 409);
         }
         const hashPassword = await bcrypt.hash(password,3);
         const activationLink = uuid.v4()
         const user = await UserModel.create({ email, password: hashPassword });
         await mailService.sendActivationMail(email, activationLink);
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {...tokens, user: userDto}
     }
 
 }
