1bb061f80e5fab711f00c8dc5006aab5a46ee74f
Vandamchik <cherepcock@gmail.com>
2023-10-27 12:39:09 +0300

    Maked logic connected with removing tokens in database, for not making trash of tokens in our database

--- utils/genTokenAndUser.js
+++ utils/genTokenAndUser.js
@@ -1,8 +1,11 @@
 const UserDto = require('../dtos/user-dto');
 const tokenService = require("../service/token-service");
+const TokenModel = require('../models/token-model');
 
 module.exports = async function (user) {
     const userDto = new UserDto(user);
+    const prevToken = await TokenModel.findOne({user: userDto.id});
+    if (!!prevToken) await TokenModel.deleteOne({user: userDto.id});
     const tokens = tokenService.generateTokens({...userDto});
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
     return {...tokens, user: userDto};
